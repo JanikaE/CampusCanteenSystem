955cfff68c07b98c019e638cb37b825d42736147
JanikaE <2272253314@qq.com>
2024-03-24 15:23:32 +0800

    实时读卡号

--- WindowFront/MainForm.Designer.cs
+++ WindowFront/MainForm.Designer.cs
@@ -28,16 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            LableTips = new Label();
+            components = new System.ComponentModel.Container();
+            TimerPort = new System.Windows.Forms.Timer(components);
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            LabelPortTips = new Label();
+            LableWebTips = new Label();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            TextBoxName = new TextBox();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
             SuspendLayout();
             // 
-            // LableTips
+            // TimerPort
             // 
-            LableTips.AutoSize = true;
-            LableTips.Location = new Point(618, 30);
-            LableTips.Name = "LableTips";
-            LableTips.Size = new Size(0, 20);
-            LableTips.TabIndex = 0;
+            TimerPort.Interval = 300;
+            TimerPort.Tick += TimerPort_Tick;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 75.90206F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 24.0979385F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 1, 1);
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.4319248F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 83.56808F));
+            tableLayoutPanel1.Size = new Size(776, 426);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(LabelPortTips, 0, 1);
+            tableLayoutPanel2.Controls.Add(LableWebTips, 0, 0);
+            tableLayoutPanel2.Location = new Point(3, 3);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 36F));
+            tableLayoutPanel2.Size = new Size(272, 64);
+            tableLayoutPanel2.TabIndex = 0;
+            // 
+            // LabelPortTips
+            // 
+            LabelPortTips.Anchor = AnchorStyles.Left;
+            LabelPortTips.AutoSize = true;
+            LabelPortTips.Location = new Point(3, 36);
+            LabelPortTips.Name = "LabelPortTips";
+            LabelPortTips.Size = new Size(70, 20);
+            LabelPortTips.TabIndex = 3;
+            LabelPortTips.Text = "PortTips";
+            LabelPortTips.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // LableWebTips
+            // 
+            LableWebTips.Anchor = AnchorStyles.Left;
+            LableWebTips.AutoSize = true;
+            LableWebTips.Location = new Point(3, 4);
+            LableWebTips.Name = "LableWebTips";
+            LableWebTips.Size = new Size(73, 20);
+            LableWebTips.TabIndex = 2;
+            LableWebTips.Text = "WebTips";
+            LableWebTips.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 1;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Controls.Add(TextBoxName, 0, 0);
+            tableLayoutPanel3.Location = new Point(592, 73);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 3;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 52.1052628F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 47.8947372F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 159F));
+            tableLayoutPanel3.Size = new Size(181, 350);
+            tableLayoutPanel3.TabIndex = 3;
+            // 
+            // TextBoxName
+            // 
+            TextBoxName.Enabled = false;
+            TextBoxName.Location = new Point(3, 3);
+            TextBoxName.Name = "TextBoxName";
+            TextBoxName.ReadOnly = true;
+            TextBoxName.Size = new Size(125, 27);
+            TextBoxName.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -45,15 +126,24 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             ClientSize = new Size(800, 450);
-            Controls.Add(LableTips);
+            Controls.Add(tableLayoutPanel1);
             Name = "MainForm";
             Text = "MainForm";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label LableTips;
+        private System.Windows.Forms.Timer TimerPort;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label LableWebTips;
+        private Label LabelPortTips;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TextBox TextBoxName;
     }
 }
--- WindowFront/MainForm.cs
+++ WindowFront/MainForm.cs
@@ -1,10 +1,16 @@
-using Common;
+using System.IO.Ports;
+using System.Windows.Forms;
 
 namespace WindowFront
 {
     public partial class MainForm : Form
     {
-        private HttpClient _httpClient;
+        private HttpClient _httpClient = new()
+        {
+            BaseAddress = new Uri(Config.Uri)
+        };
+
+        public string CardID { get; set; } = string.Empty;
 
         public MainForm()
         {
@@ -14,24 +20,217 @@ namespace WindowFront
 
         private void Initialize()
         {
-            _httpClient = new HttpClient()
-            {
-                BaseAddress = new Uri(Config.Uri)
-            };
-            CheckWeb();
+            InitWeb();
+            InitPort();
         }
 
-        private async void CheckWeb()
+        #region Web
+
+        private async void InitWeb()
         {
-            var result = await _httpClient.GetAsync("/Home/GetTime");
-            string response = await result.Content.ReadAsStringAsync();
-            if (result != null) { 
-                LableTips.Text = response.ToString();
+            try
+            {
+                var result = await _httpClient.GetAsync("/Home/GetTime");
+                string response = await result.Content.ReadAsStringAsync();
+                if (result != null)
+                {
+                    LableWebTips.Text = response.ToString();
+                }
+                else
+                {
+                    LableWebTips.Text = "null";
+                }
+            }
+            catch
+            {
+                LableWebTips.Text = "网络未连接";
+            }
+        }
+
+        #endregion
+
+        #region Port
+
+        private readonly SerialPort port = new();
+        public byte[] RevDataBuffer = new byte[30];
+        public uint RevDataBufferCount;
+
+        private void InitPort()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+            {
+                try
+                {
+                    port.PortName = ports[0];
+                    port.BaudRate = 9600;
+                    port.Open();
+                    LabelPortTips.Text = "端口正常";
+                    TimerPort.Start();
+                }
+                catch (Exception ex)
+                {
+                    LabelPortTips.Text = "端口打开失败 " + ex.Message;
+                }
             }
             else
             {
-                LableTips.Text = "null";
+                LabelPortTips.Text = "找不到端口";
             }
+        }
+
+        private void TimerPort_Tick(object sender, EventArgs e)
+        {
+            int revbuflen;
+
+            byte pkttype;
+            byte pktlength;
+            byte cmd;
+            byte err;
+
+            byte[] CardId = new byte[4];
+
+            bool revflag;
+
+            try
+            {
+                if (port.IsOpen)
+                {
+                    //读缓冲区中数据长度
+                    revbuflen = port.BytesToRead;
+                    revflag = false;
+                    //判断是否有数据
+                    if (revbuflen > 0)
+                    {
+                        revflag = true;
+                        Thread.Sleep(50);
+                    }
+                    else
+                    {
+                        ChangeCard(string.Empty);
+                    }
+                    RevDataBufferCount = 0;
+                    //从串口缓冲区把数据读入到RevDataBufferCount数组中
+                    while (revflag)
+                    {
+                        RevDataBuffer[RevDataBufferCount] = (byte)port.ReadByte();
+                        RevDataBufferCount++;
+                        //防止缓冲区溢出
+                        if (RevDataBufferCount >= 30)
+                        {
+                            RevDataBufferCount = 0;
+                        }
+                        Thread.Sleep(2);
+                        revbuflen = port.BytesToRead;
+                        if (revbuflen > 0)
+                        {
+                            revflag = true;
+                        }
+                        else
+                        {
+                            revflag = false;
+                        }
+                    }
+                    //判断是否接收到一帧数据
+                    if ((RevDataBuffer[1] <= RevDataBufferCount) && (RevDataBufferCount != 0x0))
+                    {
+                        RevDataBufferCount = 0x0;
+                        //计算校验和
+                        if (!CheckSumIn(RevDataBuffer, RevDataBuffer[1]))
+                        {
+                            ChangeCard(string.Empty);
+                            return;
+                        }
+                        //获取包类型
+                        pkttype = RevDataBuffer[0];
+                        //获取包长度
+                        pktlength = RevDataBuffer[1];
+                        //获取命令
+                        cmd = RevDataBuffer[2];
+                        //获取状态
+                        err = RevDataBuffer[4];
+
+                        if (err != 0)
+                        {
+                            ChangeCard(string.Empty);
+                            return;
+                        }
+                        //获取数据
+                        for (int i = 0; i < pktlength - 5; i++)
+                        {
+                            RevDataBuffer[i] = RevDataBuffer[i + 5];
+                        }
+                        if (pkttype == 0x04)
+                        {
+                            switch (cmd)
+                            {
+                                case 0x02:
+                                    //获取卡号
+                                    for (int i = 0; i < 4; i++)
+                                    {
+                                        CardId[i] = RevDataBuffer[i + 2];
+                                    }
+                                    string strString = ByteToHexStr(CardId, 4);
+                                    ChangeCard(strString);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LabelPortTips.Text = "串口通信异常 " + ex.Message;
+                if (port.IsOpen)
+                {
+                    port.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 数组转十六进制字符显示
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static string ByteToHexStr(byte[] bytes, int len)
+        {
+            string returnStr = "";
+            if (bytes != null)
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    returnStr += bytes[i].ToString("X2");
+                }
+            }
+            return returnStr;
+        }
+
+        public static bool CheckSumIn(byte[] buf, byte len)
+        {
+            byte i;
+            byte checksum;
+            checksum = 0;
+            for (i = 0; i < (len - 1); i++)
+            {
+                checksum ^= buf[i];
+            }
+            if (buf[len - 1] == (byte)~checksum)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+
+        private void ChangeCard(string cardID)
+        {
+            CardID = cardID;
+            TextBoxName.Text = cardID;
         }
     }
 }
